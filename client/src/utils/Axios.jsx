82946a3c76b34020830930bae01005f337a07008
Akshit <akshitgaur2003@gmail.com>
2024-01-31 22:04:25 +0530

    resolving issues

--- client/src/utils/Axios.jsx
+++ client/src/utils/Axios.jsx
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${process.env.API_KEY}`
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNDM1MjkwMjExYjhjODBhNzM3NzU3NjRiNGRiY2YxZiIsInN1YiI6IjY0ZDdhOTA2ZjQ5NWVlMDI5MjRlZGFjNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c7zDLuN0nFwWb7DhSKwM9xOfOEJRR0kl2dH3bEv9hS4'
       },
 });
 
