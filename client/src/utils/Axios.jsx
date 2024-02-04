366bcce5ae4cedd903f35f3cfd911cd5f9b69881
Akshit <akshitgaur2003@gmail.com>
2024-02-04 21:49:48 +0530

    bug fixes

--- client/src/utils/Axios.jsx
+++ client/src/utils/Axios.jsx
@@ -1,11 +1,11 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3",
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNDM1MjkwMjExYjhjODBhNzM3NzU3NjRiNGRiY2YxZiIsInN1YiI6IjY0ZDdhOTA2ZjQ5NWVlMDI5MjRlZGFjNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c7zDLuN0nFwWb7DhSKwM9xOfOEJRR0kl2dH3bEv9hS4'
-      },
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.REACT_APP_MOVIE_DB_API_KEY}`,
+  },
 });
 
 export default instance;
